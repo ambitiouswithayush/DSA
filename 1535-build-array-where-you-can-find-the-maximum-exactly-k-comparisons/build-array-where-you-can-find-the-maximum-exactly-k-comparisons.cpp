71c498be140cdb5d35d9455a199bcc41f188273b
Ayush Kumar <153111309+ambitiouswithayush@users.noreply.github.com>
2026-04-09 03:49:16 +0530

    Time: 142 ms (35.28%) | Memory: 8.8 MB (89.47%) - LeetSync

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -3,34 +3,34 @@ public:
     int N,M,K;
     int MOD=1e9+7;
     int dp[51][51][101];
-    int solve(int idx,int searchCost,int maxSoFar){
+
+    int solve(int idx,int searchCost,int maxSofar){
         if(idx==N){
             if(searchCost==K){
                 return 1;
             }
             return 0;
         }
-        if(dp[idx][searchCost][maxSoFar]!=-1){
-            return dp[idx][searchCost][maxSoFar];
+        if(dp[idx][searchCost][maxSofar]!=-1){
+            return dp[idx][searchCost][maxSofar];
         }
         int result=0;
         for(int i=1;i<=M;i++){
-            if(i>maxSoFar){
+            if(i>maxSofar){
                 result=(result+solve(idx+1,searchCost+1,i))%MOD;
             }
             else{
-                result=(result+solve(idx+1,searchCost,maxSoFar))%MOD;
+                result=(result+solve(idx+1,searchCost,maxSofar))%MOD;
             }
         }
-        return dp[idx][searchCost][maxSoFar]=result%MOD;
+        return dp[idx][searchCost][maxSofar]=result%MOD;
     }
     int numOfArrays(int n, int m, int k) {
         N=n;
         M=m;
         K=k;
-
+        
         memset(dp,-1,sizeof(dp));
-
         return solve(0,0,0);
     }
 };
