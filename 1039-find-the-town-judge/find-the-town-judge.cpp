a42a454e4ccaa6298838c7136842c8f041774f13
Ayush Kumar <153111309+ambitiouswithayush@users.noreply.github.com>
2026-04-13 02:46:48 +0530

    Time: 0 ms (100.00%) | Memory: 65 MB (82.67%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -4,16 +4,16 @@ public:
         vector<int>indegree(n+1);
         vector<int>outdegree(n+1);
 
-        for(vector<int>&vec:trust){
+        for(vector<int>&vec :trust){
             int u=vec[0];
             int v=vec[1];
 
             outdegree[u]++;
             indegree[v]++;
-        }
+        } 
         for(int i=1;i<=n;i++){
-            if(indegree[i]==n-1 &&outdegree[i]==0)
-            return i;
+            if(indegree[i]==n-1 && outdegree[i]==0)
+                return i;
         }
         return -1;
     }
