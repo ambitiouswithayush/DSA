4fca389a092e84cbbdc27d0bfd4339e7a1e9cd11
Ayush Kumar <153111309+ambitiouswithayush@users.noreply.github.com>
2026-04-13 01:25:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (47.43%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,48 +1,44 @@
 class DSU{
-    public:
+public:
     vector<int>parent;
     vector<int>rank;
-    
+
     DSU(int n){
         parent.resize(n+1);
         rank.resize(n+1);
+
         for(int i=1;i<=n;i++){
-            parent[i]=i;
-            rank[i]=0;
+           parent[i]=i;
+           rank[i]=0; 
         }
     }
     int find(int x){
         if(x==parent[x]) return x;
-        
+
         return parent[x]=find(parent[x]);
     }
-    void unite(int x, int y){
-        int parent_x=find(x);
-        int parent_y=find(y);
-        
-        if(parent_x==parent_y) return;
-        
-        if(rank[parent_x]>rank[parent_y])
-        parent[parent_y]=parent_x;
-        else if(rank[parent_x]<rank[parent_y])
-        parent[parent_x]=parent_y;
-        else{
-            parent[parent_x]=parent_y;
-            rank[parent_y]++;
-        }
+    void unite(int x,int y){
+        int x_parent=find(x);
+        int y_parent=find(y);
+
+        if(x_parent==y_parent) return;
+
+        if(rank[x_parent]>rank[y_parent])
+        parent[y_parent]=x_parent;
+        else if(rank[x_parent]<rank[y_parent])
+        parent[x_parent]=y_parent;
+        else
+        {parent[x_parent]=y_parent;
+        rank[y_parent]++;}
     }
 
 };
-
-
 class Solution {
-public: 
-    
+public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
-
+        
         DSU dsu(n);
-
         for(auto &edge:edges){
             int u=edge[0];
             int v=edge[1];
