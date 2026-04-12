f1d052bbed946efbb6791a7e12856d7db818de1b
Ayush Kumar <153111309+ambitiouswithayush@users.noreply.github.com>
2026-04-13 00:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (60.18%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.cpp
@@ -0,0 +1,57 @@
+class DSU{
+    public:
+    vector<int>parent;
+    vector<int>rank;
+    
+    DSU(int n){
+        parent.resize(n+1);
+        rank.resize(n+1);
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+            rank[i]=0;
+        }
+    }
+    int find(int x){
+        if(x==parent[x]) return x;
+        
+        return parent[x]=find(parent[x]);
+    }
+    void unite(int x, int y){
+        int parent_x=find(x);
+        int parent_y=find(y);
+        
+        if(parent_x==parent_y) return;
+        
+        if(rank[parent_x]>rank[parent_y])
+        parent[parent_y]=parent_x;
+        else if(rank[parent_x]<rank[parent_y])
+        parent[parent_x]=parent_y;
+        else{
+            parent[parent_x]=parent_y;
+            rank[parent_y]++;
+        }
+    }
+
+};
+
+
+class Solution {
+public: 
+    
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+
+        DSU dsu(n);
+
+        for(auto &edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+
+            if(dsu.find(u)==dsu.find(v))
+            return edge;
+
+            dsu.unite(u,v);
+        }
+        return {};
+    }
+};
