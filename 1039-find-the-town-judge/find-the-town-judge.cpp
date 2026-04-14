23726901a22a3f9a4b1b59f41cef19d78eb6001e
Ayush Kumar <153111309+ambitiouswithayush@users.noreply.github.com>
2026-04-15 00:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (82.82%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int>indegree(n+1);
-        vector<int>outdegree(n+1);
-
+        vector<int>count(n+1);
+       
         for(vector<int>&vec :trust){
             int u=vec[0];
             int v=vec[1];
             
-            outdegree[u]++;
-            indegree[v]++;
+            count[u]--;
+            count[v]++;
         }
         for(int i=1;i<=n;i++){
-            if(indegree[i]==n-1 && outdegree[i]==0  )
+            if(count[i]==n-1 )
                 return i;
         }
         return -1;
