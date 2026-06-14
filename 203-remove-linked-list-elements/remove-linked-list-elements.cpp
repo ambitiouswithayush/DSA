a0df9b06579828afd417185d8e9e5276a26e12fb
Ayush Kumar <153111309+ambitiouswithayush@users.noreply.github.com>
2026-06-15 00:43:01 +0530

    Time: 0 ms (100.00%) | Memory: 20.2 MB (22.52%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -11,25 +11,20 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        while(head&&head->val==val){
-            
-                ListNode* temp=head;
-                head=head->next;
-                delete temp;
-            
+        while(head && head->val==val){
+            ListNode*temp=head;
+            head=head->next;
+            delete temp;
         }
-
         ListNode*curr=head;
-
         while(curr && curr->next){
             if(curr->next->val==val){
                 ListNode*temp=curr->next;
                 curr->next=curr->next->next;
                 delete temp;
             }
-            else{
+            else
                 curr=curr->next;
-            }
         }
         return head;
     }
